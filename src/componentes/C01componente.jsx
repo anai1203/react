3234e983c3561943d30f84c538432566b343bcc2
anai1203 <anai.flor.angelica@gmail.com>
2022-07-19 15:46:42 -0500

    Privera version del sitio

---
+++ src/componentes/C01componente.jsx
@@ -0,0 +1,12 @@
+//rafce
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+        Componente definida con funcion flecha (rafce)
+    </div>
+  )
+}
+
+export default C01componente
